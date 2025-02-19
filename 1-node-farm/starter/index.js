9b29f5877c6ede4976462ea0f42bba597a655905
NATTHAPHONG RUTHAMNONG <gogozaa123_@hotmail.com>
2025-02-19 16:19:52 +0700

    create non-blocking

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// //Blocking, Synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textOut = `this is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
 
-const textOut = `this is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+//Non-Blocking, Asynchronous way
+fs.readFile('./txt/startt.txt', 'utf-8', (err, data1) => {
+  if (err) {
+    return console.log('ERROR! 💥');
+  }
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+      console.log(data3);
 
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!');
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Your file has been written 😁');
+      });
+    });
+  });
+});
+console.log('Will read file!');
